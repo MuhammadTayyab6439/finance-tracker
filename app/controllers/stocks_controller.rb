fd706266c19251ed839e49eb8ca54e20daa92689
Muhammad Tayyab <Bcsf17a512@pucit.edu.pk>
2022-02-16 17:11:33 +0500

    apply search symbol validations

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,9 +1,21 @@
 class StocksController < ApplicationController
 
     def search
+        if params[:stock].present?               
+            @stock=Stock.new_lookup(params[:stock])
 
-        @stock=Stock.new_lookup(params[:stock])
-        render 'users/my_portfolio'
+           if @stock
+            render 'users/my_portfolio'
+         
+           else
+            flash[:alert]="please enter a valid symbol"
+            redirect_to my_portfolio_path
+           end
+
+        else
+            flash[:alert]="please enter a symbol"
+            redirect_to my_portfolio_path
+        end
     end
 
 
