d3efbd83c2e125d5587f47ee3483db49891a5219
Muhammad Tayyab <Bcsf17a512@pucit.edu.pk>
2022-02-17 18:09:59 +0500

    adding complete search friend functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,24 +10,65 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-         
+  #this method return true if stock already present in db        
   def stock_already_tracked?(ticker_symbol)
     stock = Stock.check_db(ticker_symbol)
     return false unless stock
     stocks.where(id: stock.id).exists?    
   end
   
+  #this method return true if stock limit less than 10
   def under_stock_limit?
     stocks.count < 10
   end
 
+  #this method uses above two methods and return true only if stock is not present in db and stock count less than 10
   def can_track_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_tracked?(ticker_symbol)
   end
 
+
+
+  #this method provides full name by joining firstname and lastname if they present otherwise return unknown
   def fullname
     return "#{first_name} #{last_name}" if first_name || last_name
     "unknown"
   end
 
+
+
+
+  #this method get param as input from user then strip it(remove spaces) then send search result
+  #on basis of user input either it is first_name,last_name,email by calling below methods
+  def self.search(param)
+    param.strip!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
+
+  #this method calls a method matches and provide firstname as field name and param field(recieve from search method)
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+ #this method calls a method matches and provide lastname as field name and param field(recieve from search method)
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+
+ #this method calls a method matches and provide email as field name and param field(recieve from search method)
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+#this method is used in above three methods and above three methods are used in search method
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
+
+  #this method ensure that current logged in user can't show in search result
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get 'my_stocks', to: 'stocks#search'
 
   get 'my_friends',to:'users#my_friends'
+
+  get 'search_friend', to: 'users#search'
   
 end
