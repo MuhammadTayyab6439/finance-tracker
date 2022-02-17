51ed1b556ab8d8bb7ef8b92c3ffc7a5935b9a0c9
Muhammad Tayyab <Bcsf17a512@pucit.edu.pk>
2022-02-17 14:26:16 +0500

    userstock removing functionality

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -13,5 +13,14 @@ class UserStocksController < ApplicationController
             flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
             redirect_to my_portfolio_path
         end
+        def destroy
+            #first we find stock on base of id sent from list.html.erb user_stock(stock) delete method
+            stock=Stock.find(params[:id])
+            #now we find USERSTOCK on base of curent_user id and stock id of stock we find above
+            user_stock=UserStock.where(user_id: current_user.id,stock_id: stock.id).first
+            user_stock.destroy
+            flash[:notice]="stock deleted successfully"
+            redirect_to my_portfolio_path
+        end
 
 end
