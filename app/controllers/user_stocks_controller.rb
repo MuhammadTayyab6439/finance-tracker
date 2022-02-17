49273ef9f4f38635929bcad74390cb3ee2661c74
Muhammad Tayyab <Bcsf17a512@pucit.edu.pk>
2022-02-17 13:13:55 +0500

    addstock to portfolio table using userstock create action

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,17 @@
 class UserStocksController < ApplicationController
+    #action against add to portfolio button
+        def create
+            #first check stock object from db table 
+            stock = Stock.check_db(params[:ticker])
+            #if not present in db table then we get it from newlookup method and save it to db
+            if stock.blank?
+              stock = Stock.new_lookup(params[:ticker])
+              stock.save
+            end  
+              #then we pass stock obj to create userstock
+            @user_stock = UserStock.create(user: current_user, stock: stock)
+            flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
+            redirect_to my_portfolio_path
+        end
+
 end
